b4238779a27f24116af2a1e9837e6a434cbf3af1
Riccardo Vella <vella.1806809@studenti.uniroma1.it>
2020-05-08 11:36:21 +0200

    new server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use("/static",express.static('views'));
+app.use(express.static('views'));
 
 app.use(require('./controllers'));
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
 
-console.log("You are listening to port 3000");
+
