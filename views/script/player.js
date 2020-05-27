f97c641c9a73f69748979c8e09493865cbb3d12d
simonepetruzzi <simone.petruzzi98@libero.it>
2020-05-27 16:44:07 +0200

    Comments

--- views/script/player.js
+++ views/script/player.js
@@ -1,35 +1,26 @@
 // This code loads the IFrame Player API code asynchronously.
 
-var video = $('#player').attr("video"); //video url
+var video = $('#player').attr("video"); //video url taken from genius call
 if(video) {
 
     var id = filterVideoId();
     var tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
+    tag.src = "https://www.youtube.com/iframe_api";//script element and gives source 
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     // This function creates an <iframe> (and YouTube player)
     // after the API code downloads.
     var player;
-    function onYouTubeIframeAPIReady() {
+    function onYouTubeIframeAPIReady() {//load the youtube player
         player = new YT.Player('player', {
             width: '100%',
             videoId: id,
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
+            
         });
     }
-
-    // The API will call this function when the video player is ready.
-    function onPlayerReady(event) { }
-
-    // The API calls this function when the player's state changes.
-    function onPlayerStateChange(event) { }
-
+    
     // This function filters the youtube video ID from the URL
     function filterVideoId() {
         
